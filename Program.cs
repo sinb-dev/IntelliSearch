9e9af297de09457cae79d0a6a5368032222bea8b
TheOnlySlothman <lasselm013@gmail.com>
2020-03-05 12:49:24 +0100

    Added Replace Method

--- Program.cs
+++ Program.cs
@@ -9,6 +9,9 @@ namespace IntelliSearch
         {
             Switch test = new Switch();
             test.Process("test", ProcessorConfig.Instance);
+
+            Replace test2 = new Replace();
+            test2.Process("test", ProcessorConfig.Instance);
         }
     }
 
@@ -26,6 +29,29 @@ namespace IntelliSearch
                 tempArr[i] = tempArr[i + 1];
                 tempArr[i + 1] = temp;
                 result.Add(new string(tempArr));
+                Console.WriteLine(new string(tempArr));
+            }
+
+            return result;
+        }
+    }
+
+    class Replace : IWordProcessor
+    {
+        public List<string> Process(string word, ProcessorConfig config)
+        {
+            List<string> result = new List<string>();
+            char[] ChArr = config.CharacterList.ToCharArray();
+
+            for (int x = 0; x < ChArr.Length; x++)
+            {
+                for (int i = 0; i < word.Length; i++)
+                {
+                    char[] tempArr = word.ToCharArray();
+                    tempArr[i] = ChArr[x];
+                    result.Add(new string(tempArr));
+                    Console.WriteLine(new string(tempArr));
+                }
             }
 
             return result;
