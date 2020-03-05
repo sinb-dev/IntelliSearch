e554c51464897d74620fac816b4acc67df285ced
niko124a <56532129+niko124a@users.noreply.github.com>
2020-03-05 12:54:35 +0100

    Created AddChar Class

--- Program.cs
+++ Program.cs
@@ -6,7 +6,8 @@ namespace IntelliSearch
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            AddChar addChar = new AddChar();
+            addChar.Process(Console.ReadLine(), ProcessorConfig.Instance);
         }
     }
 }
