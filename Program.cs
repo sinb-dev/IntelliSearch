38ca5fc05166e368a7cf14ff8dd5f3b3993d1481
andr942f <56531655+andr942f@users.noreply.github.com>
2020-03-05 14:20:45 +0100

    Updated Layout
    Lavede lige nogle rettelser igen :)))))

--- Program.cs
+++ Program.cs
@@ -5,45 +5,42 @@ namespace IntelliSearch
 {
     class Program
     {
-        public static void Line()
+        public static string Overskrift(string skrift)
         {
+            Console.WriteLine();
+            Console.WriteLine(skrift);
             for (int x = 0; x < 50; x++)
             {
                 Console.Write("-");
             }
             Console.WriteLine();
+            return skrift;
         }
         static void Main(string[] args)
         {
             Console.WriteLine("Enter a string that will be used throughout the program");
             var input = Console.ReadLine();
-            Console.WriteLine();
 
-            Console.WriteLine("The string will be subtracted on each index");
-            Line();
+            Overskrift("Subtract");
             Sub subtract = new Sub();
             subtract.Process(input, ProcessorConfig.Instance);
 
-            Console.WriteLine();
-            Console.WriteLine("The string will add a char on each index");
-            Line();
+            Overskrift("AddChar");
             Add addChar = new Add();
             addChar.Process(input, ProcessorConfig.Instance);
 
-            Console.WriteLine();
-            Console.WriteLine("The string will replace two indexes that are next to each other");
-            Line();
+            Overskrift("Switch/Replace");
             Switch test = new Switch();
             test.Process(input, ProcessorConfig.Instance);
             Replace test2 = new Replace();
             test2.Process(input, ProcessorConfig.Instance);
+
+            Overskrift("Yes?");
             Add add = new Add();
-
             List<string> result = new List<string>();
-
             WordProcessing wordProcessing = new WordProcessing();
             wordProcessing.AddProcesser(add);
-            wordProcessing.ProcessedResults("test", ref result);
+            wordProcessing.ProcessedResults(input, ref result);
         }
     }
 }
